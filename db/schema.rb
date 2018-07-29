842a3fd3bfa7c2856707f5ff2b751d7428114e63
Alphafrau <alpha-frau@alphafrau.de>
2018-07-29 21:38:32 +0200

    erledigt formatiert

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180729131118) do
+ActiveRecord::Schema.define(version: 20180729184947) do
 
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "completed_at"
   end
 
 end
